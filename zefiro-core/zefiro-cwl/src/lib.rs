bd0391759ba10de76835bdb563fd830f46b1879d
Nikita Syzrantsev <47356892+nsyzrantsev@users.noreply.github.com>
2025-01-10 20:06:20 +0400

    Fix formatting fails

--- zefiro-core/zefiro-cwl/src/lib.rs
+++ zefiro-core/zefiro-cwl/src/lib.rs
@@ -1,8 +1,8 @@
+pub mod exprs;
 #[doc = include_str!("../README.md")]
 pub mod schema;
 pub mod values;
-pub mod exprs;
 
+pub use crate::exprs::exec::JsExecutor;
 pub use crate::schema::document::CwlSchema;
 pub use crate::values::document::CwlValues;
-pub use crate::exprs::exec::JsExecutor;
