cdb6f49de46b83debcfe143d346b4ff308fdca94
Nikita Syzrantsev <47356892+nsyzrantsev@users.noreply.github.com>
2025-01-11 13:53:04 +0400

    Add structure to render templates using tera engine

--- zefiro-core/zefiro-cwl/src/lib.rs
+++ zefiro-core/zefiro-cwl/src/lib.rs
@@ -1,8 +1,10 @@
-pub mod exprs;
 #[doc = include_str!("../README.md")]
 pub mod schema;
 pub mod values;
+pub mod js;
+pub mod template;
 
-pub use crate::exprs::exec::JsExecutor;
+pub use crate::js::execute::JsExecutor;
 pub use crate::schema::document::CwlSchema;
 pub use crate::values::document::CwlValues;
+pub use crate::template::render::TemplateRender;
